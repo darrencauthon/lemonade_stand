b6b6783031b5fb82327a0dc499f73a31bf1fe59a
Darren Cauthon <darren@cauthon.com>
2014-10-18 18:33:08 -0500

    Refactor.

--- spec/lemonade_stand/game_spec.rb
+++ spec/lemonade_stand/game_spec.rb
@@ -13,17 +13,19 @@ describe LemonadeStand::Game do
 
         let(:game) { LemonadeStand::Game.new example.player_count }
 
-        it "should require the number of players" do
-          game.players.count.must_equal example.player_count
-        end
+        describe "players" do
+          it "should require the number of players" do
+            game.players.count.must_equal example.player_count
+          end
 
-        it "should return player objects for each player" do
-          game.players.each { |p| p.is_a?(LemonadeStand::Player).must_equal true }
-        end
+          it "should return player objects for each player" do
+            game.players.each { |p| p.is_a?(LemonadeStand::Player).must_equal true }
+          end
 
-        it "should set the player index on each as an incrementing index" do
-          game.players.each_with_index do |player, index|
-            player.index.must_equal index
+          it "should set the player index on each as an incrementing index" do
+            game.players.each_with_index do |player, index|
+              player.index.must_equal index
+            end
           end
         end
 
