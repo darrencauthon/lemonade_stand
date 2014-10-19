bf1dd84cf5348aa2e435aa0bf19a59bf520db709
Darren Cauthon <darren@cauthon.com>
2014-10-19 08:46:45 -0500

    Rename the method, pass in the day.

--- lib/lemonade_stand/day.rb
+++ lib/lemonade_stand/day.rb
@@ -3,7 +3,7 @@ module LemonadeStand
   class Day
 
     def projected_weather_report
-      @projected_weather_report ||= LemonadeStand::Weather.build_projected_weather_report
+      @projected_weather_report ||= LemonadeStand::Weather.projected_weather_for(self)
     end
 
   end
--- spec/lemonade_stand/day_spec.rb
+++ spec/lemonade_stand/day_spec.rb
@@ -8,13 +8,13 @@ describe LemonadeStand::Day do
     
     it "should pull a projected weather report" do
       report = Object.new
-      LemonadeStand::Weather.stubs(:build_projected_weather_report).returns report
+      LemonadeStand::Weather.stubs(:projected_weather_for).with(day).returns report
       day.projected_weather_report.must_be_same_as report
     end
 
     it "should return the same projected weather report" do
       report = Object.new
-      LemonadeStand::Weather.expects(:build_projected_weather_report).returns report
+      LemonadeStand::Weather.expects(:projected_weather_for).returns report
       day.projected_weather_report
       day.projected_weather_report # this call will fail if called twice
     end
