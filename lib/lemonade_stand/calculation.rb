99939c00e022aec7839799eaf8ef2c2249c2344e
Darren Cauthon <darren@cauthon.com>
2014-10-19 10:33:51 -0500

    Base sales.

--- lib/lemonade_stand/calculation.rb
+++ lib/lemonade_stand/calculation.rb
@@ -5,16 +5,23 @@ module LemonadeStand
     DEMAND     = 30.0
 
     def self.sales_factor choice
-      result = if choice.price_per_glass < 10
-                 (HIGH_PRICE - choice.price_per_glass) / HIGH_PRICE * 0.8 * DEMAND + DEMAND
+      price_per_glass = choice.price_per_glass
+      result = if price_per_glass < 10
+                 (HIGH_PRICE - price_per_glass) / HIGH_PRICE * 0.8 * DEMAND + DEMAND
                else
-                 (HIGH_PRICE * HIGH_PRICE) * DEMAND / (choice.price_per_glass * choice.price_per_glass)
+                 (HIGH_PRICE * HIGH_PRICE) * DEMAND / (price_per_glass * price_per_glass)
                end
       result
     end
 
     def self.signs_factor choice
-      1 - Math.exp((-1 * choice.signs) * 0.5)
+      1.0 - Math.exp((-1.0 * choice.signs) * 0.5)
+    end
+
+    def self.base_sales choice
+      sales_factor = sales_factor choice
+      signs_factor = signs_factor choice
+      sales_factor + (sales_factor * signs_factor)
     end
 
   end
--- spec/lemonade_stand/calculation_spec.rb
+++ spec/lemonade_stand/calculation_spec.rb
@@ -65,4 +65,33 @@ describe LemonadeStand::Calculation do
 
   end
 
+  describe "base sales" do
+    [
+      [1,    0,   51.60],
+      [1,    1,   71.90],
+      [1,    2,   84.22],
+      [1,  100,  103.20],
+      [9,    0,   32.40],
+      [9,    0,   32.40],
+      [9,    1,   45.15],
+      [9,    5,   62.14],
+      [9,   10,   64.58],
+      [9,   100,  64.80],
+    ].map { |x| Struct.new(:price_per_glass, :signs, :expected).new(*x) }.each do |example|
+
+      describe "#{example.price_per_glass} glasses with #{example.signs} signs" do
+
+        let(:choice) { example }
+
+        it "should return sales of #{example.expected}" do
+          result = LemonadeStand::Calculation.base_sales choice
+          result.round(2).must_equal example.expected
+        end
+
+      end
+
+    end
+
+  end
+
 end
