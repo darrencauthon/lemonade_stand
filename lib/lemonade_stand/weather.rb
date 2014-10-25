af7e492c2f0dafec4f1719a343e4747c4ded3f5a
Darren Cauthon <darren@cauthon.com>
2014-10-25 10:59:04 -0500

    Refactoring.

--- lib/lemonade_stand/weather.rb
+++ lib/lemonade_stand/weather.rb
@@ -1,14 +1,30 @@
 module LemonadeStand
-  module Weather
+
+  class Weather
+
+    def initialize type
+      @type = type
+    end
+
+    def method_missing(meth, *args, &blk)
+      meth.to_s == "#{@type}?"
+    end
 
     def self.weather_for day
-      sunny = day.number < 3
-      random_value = rand(10)
-      cloudy = !sunny && (random_value == 6 || random_value == 7)
-      hot_and_dry = !sunny && !cloudy && (random_value == 8 || random_value == 9)
-      sunny = !cloudy & !hot_and_dry
-      Struct.new(:sunny?, :cloudy?, :hot_and_dry?).new sunny, cloudy, hot_and_dry
+      if day.number < 3
+        new :sunny
+      else
+        random_value = rand(10)
+        if random_value < 6
+          new :sunny
+        elsif random_value < 8
+          new :cloudy
+        else
+          new :hot_and_dry
+        end
+      end
     end
 
   end
+
 end
