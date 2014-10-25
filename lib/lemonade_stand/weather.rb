c9fadf686289c076569a2b1b213d9ae0c3b00945
Darren Cauthon <darren@cauthon.com>
2014-10-25 10:06:13 -0500

    Implement different types of weather.

--- lib/lemonade_stand/weather.rb
+++ lib/lemonade_stand/weather.rb
@@ -1,4 +1,14 @@
 module LemonadeStand
   module Weather
+
+    def self.weather_for day
+      sunny = day.number < 3
+      random_value = rand(10)
+      cloudy = !sunny && (random_value == 6 || random_value == 7)
+      hot_and_dry = !sunny && !cloudy && (random_value == 8 || random_value == 9)
+      sunny = !cloudy & !hot_and_dry
+      Struct.new(:sunny?, :cloudy?, :hot_and_dry?).new sunny, cloudy, hot_and_dry
+    end
+
   end
 end
