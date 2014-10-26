f4227c3d7b2366d33efa2f75da5dccccd15e34b8
Darren Cauthon <darren@cauthon.com>
2014-10-25 19:59:04 -0500

    Refactor.

--- lib/lemonade_stand/result.rb
+++ lib/lemonade_stand/result.rb
@@ -11,8 +11,9 @@ module LemonadeStand
     end
 
     def expenses
-      result = @choice.glasses_made * @day.cost_per_glass + 15 * @choice.signs
-      (result / 100.0).round(2)
+      glasses_cost = @choice.glasses_made * @day.cost_per_glass
+      signs_cost   = 15 * @choice.signs
+      ((glasses_cost + signs_cost) / 100.0).round 2
     end
 
   end
