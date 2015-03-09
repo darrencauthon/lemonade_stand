2c097116d4b85a4d84c3ec38880ef13334f7d2e8
Darren Cauthon <darren@cauthon.com>
2015-03-09 06:23:32 -0500

    Initialize the variable in one place.

--- lib/lemonade_stand/game.rb
+++ lib/lemonade_stand/game.rb
@@ -27,13 +27,11 @@ module LemonadeStand
     end
 
     def store_sales_results_for results, player, day
-      @sales_results ||= []
-      @sales_results << { player: player, day: day, results: results }
+      sales_results << { player: player, day: day, results: results }
     end
 
     def sales_results_for player, day
-      @sales_results ||= []
-      @sales_results.select do |record|
+      sales_results.select do |record|
         record[:player].object_id == player.object_id && record[:day].object_id == day.object_id
       end.first[:results]
     end
@@ -45,6 +43,12 @@ module LemonadeStand
                        p.game  = self
                      end
                    end
+    end
+
+    private
+
+    def sales_results
+      @sales_results ||= []
     end
 
   end
