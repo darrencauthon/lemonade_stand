8cd33d2b9e7fe5a1fd91a69175391fe7a0cda2df
Dave Woodall <dave.woodall@livingsocial.com>
2015-12-22 22:46:58 -0700

    CLI#PLAY changes + supporting edits
    ## Discussion of methods in #play
    
    ### load_yaml
    Let’s us manage code and copy separately.
    
    ### set_players_and_days
    Already feels like a temporary stop in order to thin out #play.
    
    ### @gamemaster.start_game
    I like the idea of letting the playing be directed by the gamemaster
    
    ### end_game
    A way for us to reconcile and determine the winner.
    
    ## play_ls.rb
    Not sure the argv is relevant here.
    
    ## game.rb
    Moved some of the content guts out of #play

--- lib/lemonade_stand/cli.rb
+++ lib/lemonade_stand/cli.rb
@@ -1,60 +1,62 @@
+require 'yaml'
+
 module LemonadeStand
 
   class CLI
 
-    def initialize argv
-      @argv = argv
-    end
-
     def play
-
-      if @argv.include?('-v')
-        puts LemonadeStand::VERSION
-        exit
-      end
-
-      puts 'How many players are playing?'
-      number_of_players = gets.chomp.to_i
-
-      puts 'How many days will this game last?'
-      number_of_days = gets.chomp.to_i
-
-      game = LemonadeStand::Game.new number_of_players
-
-      (1..number_of_days).each do |day|
-
-        game.start_a_new_day
-
-        game.players.each do |player|
-          puts "Player #{player.index} is up!!!"
-
-          puts "You have #{player.assets} pennies."
-
-          choice = LemonadeStand::Choice.new
-
-          puts "How many glasses of lemonade do you want to make?"
-          choice.glasses_made = gets.chomp.to_i
-
-          puts "How much will you charge per glass?"
-          choice.price_per_glass = gets.chomp.to_i
-
-          player.choose choice
-
-        end
-
-        game.players.each do |player|
-          puts "Player #{player.index} ended the day with: #{player.assets}"
-        end
-
-      end
-
-      puts "The game is over!"
-      winner = game.players.sort_by { |x| x.assets }[-1]
-
-      puts "The winner is: Player #{winner.index}"
-
+      load_yaml
+      set_players_and_days
+      @gamemaster.start_game
+      end_game
     end
 
-  end
+  private
 
+    def end_game
+      LemonadeStand::Audit.new(@gamemaster).squeeze
+    end
+
+    def set_players_and_days
+      `say "#{@text['welcome']['one']}"`
+      text_formatter_with_prompt_and_sound(@text['setup']['player_setup'])
+      @number_of_players = gets.chomp
+      @number_of_players = validate @number_of_players
+      @gamemaster = LemonadeStand::GameMaster.new(@number_of_players)
+
+      text_formatter_with_prompt_and_sound(@text['setup']['length_of_game'])
+      @number_of_days = gets.chomp
+      @number_of_days = validate @number_of_days
+      @gamemaster.total_days @number_of_days
+    end
+
+    def text_formatter_with_prompt_and_sound(question)
+      puts question
+      print " > "
+      `say "#{question}"`
+    end
+
+    def load_yaml
+      filepath = File.join(File.dirname(__FILE__),"../yaml/audio_script.yaml")
+      @text = YAML.load_file(filepath)
+    end
+
+    def validate entry
+      while is_a_letter? entry
+        puts "#{entry} is not a number."
+         `say Error please enter a number`
+
+        print " > "
+        entry = gets.chomp
+        @foo = entry.to_i
+      end
+      entry.to_i || @foo
+    end
+
+    def is_a_letter? entry
+      !(entry.to_i.to_s == entry)
+    end
+  end
 end
+
+
--- lib/lemonade_stand/game.rb
+++ lib/lemonade_stand/game.rb
@@ -6,6 +6,34 @@ module LemonadeStand
       @number_of_players = number_of_players
     end
 
+    def play days
+      # (1..days).each do |day|
+
+        # start_a_new_day
+
+        # players.each do |player|
+        #   puts "Player #{player.index} is up!!!"
+
+        #   puts "You have #{player.assets} pennies."
+
+        #   choice = LemonadeStand::Choice.new
+
+        #   puts "How many glasses of lemonade do you want to make?"
+        #   choice.glasses_made = gets.chomp.to_i
+
+        #   puts "How much will you charge per glass?"
+        #   choice.price_per_glass = gets.chomp.to_i
+
+        #   player.choose choice
+
+        # end
+
+        # players.each do |player|
+        #   puts "Player #{player.index} ended the day with: #{player.assets}"
+        # end
+      # end
+    end
+
     def make_choice choice, options
       player  = options[:player]
       day     = options[:day] || days.last
@@ -37,7 +65,7 @@ module LemonadeStand
     end
 
     def players
-      @players ||= (0...@number_of_players).map do |i|
+      @players ||= (0...@number_of_players).each do |i|
                      LemonadeStand::Player.new.tap do |p|
                        p.index = i
                        p.game  = self
