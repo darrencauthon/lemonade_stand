689a3d7d09406d773c91a224973523415f914db3
Dave Woodall <dave.woodall@livingsocial.com>
2015-12-23 20:00:29 -0700

    Question about instance methods in an initializer

--- lib/lemonade_stand/game.rb
+++ lib/lemonade_stand/game.rb
@@ -1,84 +1,68 @@
 module LemonadeStand
-
   class Game
 
-    def initialize number_of_players
-      @number_of_players = number_of_players
+    attr_reader :gamemaster
+
+    def initialize gamemaster
+      @gamemaster = gamemaster
     end
 
     def play days
-      # (1..days).each do |day|
-
-        # start_a_new_day
-
-        # players.each do |player|
-        #   puts "Player #{player.index} is up!!!"
-
-        #   puts "You have #{player.assets} pennies."
-
-        #   choice = LemonadeStand::Choice.new
-
-        #   puts "How many glasses of lemonade do you want to make?"
-        #   choice.glasses_made = gets.chomp.to_i
-
-        #   puts "How much will you charge per glass?"
-        #   choice.price_per_glass = gets.chomp.to_i
-
-        #   player.choose choice
-
-        # end
-
-        # players.each do |player|
-        #   puts "Player #{player.index} ended the day with: #{player.assets}"
-        # end
-      # end
-    end
-
-    def make_choice choice, options
-      player  = options[:player]
-      day     = options[:day] || days.last
-      results = day.sales_for choice
-
-      player.assets += results.profit
-      store_sales_results_for results, player, day
-    end
-
-    def days
-      @days ||= []
-    end
-
-    def start_a_new_day
-      LemonadeStand::Day.new.tap do |day|
-        days << day
-        day.number = days.count
+      gamemaster.rounds_to_play.times do |day|
+        puts LemonadeStand::Day.new(day, days).forecast
+        gamemaster.all_players.each do |player|
+          player_turn player
+        end
+        gamemaster.remaining_rounds(day - 1) # _dw FIX
       end
     end
 
-    def store_sales_results_for results, player, day
-      sales_results << { player: player, day: day, results: results }
+    def player_turn player
+      puts "#{player.name} is up."
+      puts "You have a balance of #{player.balance}."
+      player_move_dw(player)
     end
 
-    def sales_results_for player, day
-      sales_results.select do |record|
-        record[:player].object_id == player.object_id && record[:day].object_id == day.object_id
-      end.first[:results]
+    def player_move_dw player # _dw temp method
+      @choice ||= LemonadeStand::Choice.new(player)
+      # puts "How many glasses of lemonade do you want to make?"
+      # choice.glasses_made = gets.chomp.to_i
+      # puts "How much will you charge per glass?"
+      # choice.price_per_glass = gets.chomp.to_i
+      # player.choose choice
     end
 
-    def players
-      @players ||= (0...@number_of_players).each do |i|
-                     LemonadeStand::Player.new.tap do |p|
-                       p.index = i
-                       p.game  = self
-                     end
-                   end
-    end
+    # def days
+    #   @days ||= []
+    # end
 
-    private
+    # def start_a_new_day
+    #   LemonadeStand::Day.new.tap do |day|
+    #     days << day
+    #     day.number = days.count
+    #   end
+    # end
 
-    def sales_results
-      @sales_results ||= []
-    end
+    # def store_sales_results_for results, player, day
+    #   sales_results << { player: player, day: day, results: results }
+    # end
 
+    # def sales_results_for player, day
+    #   sales_results.select do |record|
+    #     record[:player].object_id == player.object_id && record[:day].object_id == day.object_id
+    #   end.first[:results]
+    # end
+
+    # def players
+    #   @players ||= (0...@number_of_players).each do |i|
+    #                  LemonadeStand::Player.new(i, self)
+    #                end
+    # end
+
+    # private
+
+    # def sales_results
+    #   @sales_results ||= []
+    # end
   end
-
 end
--- lib/lemonade_stand/game_master.rb
+++ lib/lemonade_stand/game_master.rb
@@ -1,23 +1,79 @@
 module LemonadeStand
   class GameMaster
 
-    attr_reader :players, :game, :group
+    def initialize(config)
+      # _dw how can I access these? for some reason, attr_reader wasn't working so I created these two methods rounds_to_play and all_players
 
-    def initialize(players=1)
       @group = []
-      @players = players
+      @all_rounds = []
+      @players = config[:players]
+      @rounds = config[:rounds]
       add_players_to_group
+      create_rounds
       setup_game
     end
 
+    ## I thought an attr_reader would work
+
+    def rounds_to_play
+      @rounds
+    end
+
+    def all_players
+      @group
+    end
+
+    def all_rounds_to_pay
+      @all_rounds
+    end
+
+    def start_game
+      @game.play @days
+    end
+
+    def setup_game
+      @game ||= LemonadeStand::Game.new with_a_gamemaster
+    end
+
+    def with_a_gamemaster
+      self
+    end
+
+    def round(id)
+      id -= 1
+      @all_rounds[id]
+    end
+
+    def remaining_rounds(round)
+        @remaining_rounds = @rounds - round
+    end
+
+    def current_round
+      @all_rounds.count - @remaining_rounds
+    end
+
+    def rounds_played
+      # _dw TODO
+    end
+
     def add_players_to_group
-      players.times.each do |player, index|
+      @players.times.each do |player, index|
         @group << create_player(index)
       end
     end
 
+    def create_rounds
+      @rounds.times do |round|
+        build_round(round)
+      end
+    end
+
+    def build_round round
+      @all_rounds << LemonadeStand::Round.new(round)
+    end
+
     def total_days days
-      @days = days
+      @rounds = days
     end
 
     def rounds
@@ -26,14 +82,6 @@ module LemonadeStand
 
     def create_player index
       Player.new(index)
-    end
-
-    def start_game
-      @game.play @days
-    end
-
-    def setup_game
-      @game ||= LemonadeStand::Game.new self
     end
   end
 end
