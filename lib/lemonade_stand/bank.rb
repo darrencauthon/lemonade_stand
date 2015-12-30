e1a6e67507cbcc1c9901cf8e367c643ea5298175
Dave Woodall <dave.woodall@livingsocial.com>
2015-12-29 19:34:41 -0700

    Add methods to Bank to manage transactions

--- lib/lemonade_stand/bank.rb
+++ lib/lemonade_stand/bank.rb
@@ -1,11 +1,46 @@
 module LemonadeStand
   class Bank
+
     def initialize(assets)
       @assets = assets
+      @transactions = []
+    end
+
+    def deposit(amount)
+      @transactions << transaction('debit', amount)
+    end
+
+    def last_transaction
+      {
+        transaction: @transactions.last.transaction,
+        amount: @transactions.last.amount
+      }
     end
 
     def balance
+      transactions = @transactions.inject([]) do |memo, t|
+        memo << t.amount
+        memo
+      end
+      transactions << current_balance
+      transactions.reduce(:+)
+    end
+
+    def money
+      (@assets.to_f / 100.to_f).round(2)
+    end
+
+  private
+
+    def current_balance
       @assets
+    end
+
+    def transaction(transaction, amount)
+      @transaction ||= LemonadeStand::Transaction.new({
+        transaction: transaction,
+        amount: amount
+        })
     end
   end
 end
--- spec/lemonade_stand/bank_spec.rb
+++ spec/lemonade_stand/bank_spec.rb
@@ -5,4 +5,22 @@ describe LemonadeStand::Bank do
     bank = LemonadeStand::Bank.new(20)
     assert_equal bank.balance, 20
   end
+
+  it 'manages cents' do
+    bank = LemonadeStand::Bank.new(200)
+    assert_equal 2.00, bank.money
+  end
+
+  it 'deposits add to balance' do
+    bank = LemonadeStand::Bank.new(200)
+    bank.deposit(500)
+    assert_equal 700, bank.balance
+  end
+
+  it "remembers last transaction" do
+    bank = LemonadeStand::Bank.new(200)
+    bank.deposit(500)
+    hash = { transaction: 'debit', amount: 500 }
+    assert_equal bank.last_transaction, hash
+  end
 end
